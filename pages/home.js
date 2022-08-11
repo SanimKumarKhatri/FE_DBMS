a139ade859697f846c33aef08236c7f8903ce160
Sanim Khatri <076bei037.sanim@pcampus.edu.np>
2022-08-11 14:13:54 +0545

    Create home.js

---
+++ pages/home.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+function  home() {
+    return (
+        <>
+        <Navbar />
+        <div>Welcome to Next.js!</div>
+        </>
+    )
+  }
+  
+  export default home;
