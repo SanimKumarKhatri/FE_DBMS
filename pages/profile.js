54037a4a9e6d0ae159007a7e4dd309875764f779
Sanim Khatri <076bei037.sanim@pcampus.edu.np>
2022-08-15 20:29:43 +0545

    edited profile section

--- pages/profile.js
+++ pages/profile.js
@@ -1,30 +1,29 @@
 
  import Navbar from '../components/navbar'
  import Homestyles from '../styles/Home.module.css'
+ import React from 'react';
  import { getUserProfile, authInitialProps} from '../lib/auth';
  
-function  Profile(props) {
-    return (
-        <div className={Homestyles.container}>
-            <Navbar {...props}/>
-            <h1> Welcome {props.auth? props.auth.name : 'Guest'} </h1>
-        </div>
-    )
-  }
-  
-  Profile.getInitialProps = authInitialProps();
-
-  export default Profile;
-   /*function Profile {
+ export default class Profile extends React.Component{
 
     state ={
         user: null
     };
-    getUserProfile()
+
+    componentDidMount(){
+        getUserProfile().then(user => this.setState({user}));
+    }
+
+    render(){
     return (
         <>
-        <Navbar />
-        <pre>{JSON.stringify(this.state.user,null,2)}</pre>
+        <div className={Homestyles.container}>
+            <Navbar {...this.props}/>
+            <pre>{JSON.stringify(this.state.user,null,2)}</pre>
+        </div>
         </>
     )
-  }*/
+    }
+  }
+
+  Profile.getInitialProps = authInitialProps();
