320342a132b537b06397d92003e1b72fae1dc6c1
Sanim Khatri <076bei037.sanim@pcampus.edu.np>
2022-08-17 23:21:16 +0545

    Update index.js

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -9,10 +9,29 @@
 
     state ={
         user: [], 
-        subject: []
+        subject: [],
+        whoami: 'I dont know',
+        error: ''
     };
 
     componentDidMount(){
+        React.useEffect(()=>{
+            fetch("http://localhost:8000/account/whoami/",{
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                credentials: "include"
+            })
+            .then(response=> response.json())
+            .then((data)=>{
+                this.state.whoami=data.username;
+            })
+            .catch((err)=>{
+                console.log(err);
+                this.state.error="you arenot logged in";
+            })
+        })
+        //getting the data of user
         getUserProfile().then(user => this.setState({user}));
         if(this.props.auth.role === 'student'){
             getUsersubject().then((data)=>{
@@ -31,7 +50,7 @@
         <div className={Homestyles.container}>
             <Navbar {...this.props}/>
             <div>
-            <h2>Welcome <strong>{this.props.auth.name}</strong></h2>
+            <h2>Welcome <strong>{this.state.whoami}</strong></h2>
             <p>Role: {this.props.auth.role}</p>
             {this.state.subject.map((data,key)=>(
                 <div class="sub-content">
